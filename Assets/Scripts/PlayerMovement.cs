84e5eb578742a6d3972d1a533f9dd6ba9a880638
NguyenVanDuy20207666 <duytp8c1@gmail.com>
2024-11-07 07:39:47 +0700

    Add moving and falling logic

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,19 +6,26 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     Vector2 moveInput;
+    Rigidbody2D myRb;
+    [SerializeField] float playerSpeed = 3.0f;
     void Start()
     {
-        
+        myRb = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        
+        Run();
     }
 
     void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
-        Debug.Log(moveInput);
+    }
+
+    void Run()
+    {
+        Vector2 playerVelocity = new Vector2(moveInput.x * playerSpeed, myRb.velocity.y);
+        myRb.velocity = playerVelocity;
     }
 }
