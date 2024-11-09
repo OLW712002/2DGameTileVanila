19f3b6a06335226c56ac2f9632fb62526e0df698
NguyenVanDuy20207666 <duytp8c1@gmail.com>
2024-11-09 11:43:12 +0700

    Fix WallStuck

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,6 +8,7 @@ public class PlayerMovement : MonoBehaviour
     Vector2 moveInput;
     Rigidbody2D myRb;
     Animator myAnimator;
+    CapsuleCollider2D myCollider;
 
     [SerializeField] float playerSpeed = 3.0f;
     [SerializeField] float playerJumpForce = 5.0f;
@@ -16,11 +17,13 @@ public class PlayerMovement : MonoBehaviour
     {
         myRb = GetComponent<Rigidbody2D>();
         myAnimator = GetComponent<Animator>();
+        myCollider = GetComponent<CapsuleCollider2D>();
     }
 
     void Update()
     {
         Run();
+        AvoidWallStuck();
         ChangeDirection();
     }
 
@@ -36,9 +39,11 @@ public class PlayerMovement : MonoBehaviour
 
     void Run()
     {
+        //Running logical
         Vector2 playerVelocity = new Vector2(moveInput.x * playerSpeed, myRb.velocity.y);
         myRb.velocity = playerVelocity;
 
+        //Running animation
         bool playerHasHorizontalSpeed = Mathf.Abs(myRb.velocity.x) > Mathf.Epsilon;
         myAnimator.SetBool("isRunning", playerHasHorizontalSpeed);
     }
@@ -47,5 +52,15 @@ public class PlayerMovement : MonoBehaviour
     {
         bool playerHasHorizontalSpeed = Mathf.Abs(myRb.velocity.x) > Mathf.Epsilon;
         if (playerHasHorizontalSpeed) transform.localScale = new Vector2(Mathf.Sign(myRb.velocity.x), 1f);
+    }
+
+    void AvoidWallStuck()
+    {
+        float forceAmount = 50.0f;
+        float rayCheckWallDistance = myCollider.size.x / 2 + Mathf.Epsilon;
+        Vector2 rayDirection = new Vector2(myRb.velocity.x, 0f).normalized;
+        LayerMask layerMask = LayerMask.GetMask("Platform");
+        Vector2 rayLocation = new Vector2(transform.position.x, transform.position.y - myCollider.size.y / 2);
+        if (Physics2D.Raycast(rayLocation, rayDirection, rayCheckWallDistance, layerMask)) myRb.AddForce(-rayDirection * forceAmount);
     }
 }
