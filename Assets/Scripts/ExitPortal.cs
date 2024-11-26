a44aff1b3f12334d5a7cca7a50bebc161a9ec9ba
NguyenVanDuy20207666 <duytp8c1@gmail.com>
2024-11-26 12:10:18 +0700

    A bit change of exitportal

--- Assets/Scripts/ExitPortal.cs
+++ Assets/Scripts/ExitPortal.cs
@@ -5,6 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class ExitPortal : MonoBehaviour
 {
+    [SerializeField] float delayTime = 1.0f;
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Player")
@@ -16,7 +17,8 @@ public class ExitPortal : MonoBehaviour
     IEnumerator NextLevel()
     {
         Debug.Log("Congratulation");
-        yield return new WaitForSecondsRealtime(1.0f);
+        yield return new WaitForSecondsRealtime(delayTime);
+        if (SceneManager.GetActiveScene().buildIndex == SceneManager.sceneCountInBuildSettings) SceneManager.LoadScene(0);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
