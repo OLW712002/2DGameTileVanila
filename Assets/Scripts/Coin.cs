e9dd4920d1d6598280a43520af247f55bbcfd9b7
NguyenVanDuy20207666 <duytp8c1@gmail.com>
2024-11-29 02:42:28 +0700

    Add coin collecting sfx

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
+    [SerializeField] AudioClip coinCollectedSound;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Player")
         {
             Destroy(gameObject);
             FindObjectOfType<GameSession>().CoinPickup();
+            AudioSource.PlayClipAtPoint(coinCollectedSound, gameObject.transform.position, 1.0f);
         }
     }
 }
