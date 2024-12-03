82fe1039793563d8875e335e18218c79f07ac450
NguyenVanDuy20207666 <duytp8c1@gmail.com>
2024-12-04 00:36:08 +0700

    Update DetectPlayer for Slime

--- Assets/Scripts/SlimeMovement.cs
+++ Assets/Scripts/SlimeMovement.cs
@@ -6,33 +6,37 @@ using UnityEngine;
 public class SlimeMovement : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 1.0f;
-    [SerializeField] float boostSpeed = 2.0f;
+    [SerializeField] float boostSpeedMultiple = 2.0f;
     [SerializeField] float distanceCheck = 10.0f;
 
     Rigidbody2D myRb;
-    BoxCollider2D myFeetCollider;
 
     void Start()
     {
         myRb = GetComponent<Rigidbody2D>();
-        myFeetCollider = GetComponent<BoxCollider2D>();
     }
 
     void Update()
     {
+        myRb.velocity = new Vector2(moveSpeed, 0f);
         CheckPlayer();
     }
 
     void CheckPlayer()
     {
-        RaycastHit2D hit = Physics2D.Raycast((Vector2)myRb.transform.position, (Vector2)myRb.velocity.normalized, distanceCheck, LayerMask.GetMask("Player"));
-        Debug.DrawLine((Vector2)myRb.transform.position, (Vector2)myRb.transform.position + myRb.velocity.normalized * distanceCheck, Color.red);
+        RaycastHit2D hit = Physics2D.Raycast((Vector2)myRb.transform.position, (Vector2)myRb.velocity.normalized, distanceCheck, LayerMask.GetMask("Player", "Platform"));
+        //Debug.DrawLine((Vector2)myRb.transform.position, (Vector2)myRb.transform.position + myRb.velocity.normalized * distanceCheck, Color.red);
         if (hit.collider != null && hit.collider.CompareTag("Player"))
         {
-            myRb.velocity = new Vector2(boostSpeed, 0f);
+            myRb.velocity = myRb.velocity * boostSpeedMultiple;
+            FindObjectOfType<Animator>().speed = boostSpeedMultiple;
             Debug.Log("DetectPlayer");
         }
-        else myRb.velocity = new Vector2(moveSpeed, 0f);
+        else
+        {
+            myRb.velocity = new Vector2(moveSpeed, 0f);
+            FindObjectOfType<Animator>().speed = 1.0f;
+        }
     }
 
     void OnTriggerExit2D(Collider2D collision)
