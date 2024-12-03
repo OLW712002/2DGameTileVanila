4adcaee7d5549955c64e1e5ac15ed5f39476ed48
NguyenVanDuy20207666 <duytp8c1@gmail.com>
2024-12-03 08:45:06 +0700

    Add boostSpeed for slime

--- Assets/Scripts/SlimeMovement.cs
+++ Assets/Scripts/SlimeMovement.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 public class SlimeMovement : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 1.0f;
+    [SerializeField] float boostSpeed = 2.0f;
+    [SerializeField] float distanceCheck = 10.0f;
 
     Rigidbody2D myRb;
     BoxCollider2D myFeetCollider;
@@ -18,7 +20,19 @@ public class SlimeMovement : MonoBehaviour
 
     void Update()
     {
-        myRb.velocity = new Vector2(moveSpeed, 0f);
+        CheckPlayer();
+    }
+
+    void CheckPlayer()
+    {
+        RaycastHit2D hit = Physics2D.Raycast((Vector2)myRb.transform.position, (Vector2)myRb.velocity.normalized, distanceCheck, LayerMask.GetMask("Player"));
+        Debug.DrawLine((Vector2)myRb.transform.position, (Vector2)myRb.transform.position + myRb.velocity.normalized * distanceCheck, Color.red);
+        if (hit.collider != null && hit.collider.CompareTag("Player"))
+        {
+            myRb.velocity = new Vector2(boostSpeed, 0f);
+            Debug.Log("DetectPlayer");
+        }
+        else myRb.velocity = new Vector2(moveSpeed, 0f);
     }
 
     void OnTriggerExit2D(Collider2D collision)
